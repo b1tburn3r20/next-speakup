7b13b01bf8fda99cf5050bd7a22d6e8d6c83f2f7
b1tburn3r20 <alejandrojosuebermudez@gmail.com>
2023-11-08 14:09:01 -0600

    added backbone to a few crucial pages

---
+++ pages/importantEvents/PresidentialElection2024.jsx
@@ -0,0 +1,15 @@
+export default function PresidentialElection2024() {
+  return (
+    <main>
+      <header>Presidential Election 2024</header>
+      <div>
+        <h1>Democratic Candidates</h1>
+        <h3>List Candidates Here</h3>
+      </div>
+      <div>
+        <h1>Republican Candidates</h1>
+        <h3>List Candidates Here</h3>
+      </div>
+    </main>
+  );
+}
